0753269ef55eda8cf592e96783d53997ad653cd2
LordKajoc <rizkinuriman@upi.edu>
2023-03-17 17:16:06 +0700

    15 Maret-Replika Beranda Shopee

---
+++ app/src/main/java/com/binar/t5latihan15maret/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.binar.t5latihan15maret
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_linear_replika)
+    }
+}
